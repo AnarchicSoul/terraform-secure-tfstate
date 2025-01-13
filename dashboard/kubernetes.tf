b09c5ce43f57f74eae4bef9a979304ae4b3087bb
AnarchicSoul <duval.johan.91@gmail.com>
2025-01-13 22:13:30 +0100

    modified:   dashboard/kubernetes.tf

--- dashboard/kubernetes.tf
+++ dashboard/kubernetes.tf
@@ -18,6 +18,22 @@ resource "kubernetes_secret" "oauth2_proxy_token" {
   type = "kubernetes.io/service-account-token"
 }
 
+resource "kubernetes_cluster_role_binding" "oauth2_proxy_admin" {
+  metadata {
+    name = "oauth2-proxy-admin-binding"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin" # Accorde les droits d'administrateur
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.oauth2_proxy.metadata[0].name
+    namespace = var.namespace
+  }
+}
+
 data "kubernetes_secret" "oauth2_proxy_token" {
   depends_on = [kubernetes_secret.oauth2_proxy_token]
 
