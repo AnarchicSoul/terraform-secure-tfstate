f7c19ed3025335811d47bf75da3cee9513964794
AnarchicSoul <duval.johan.91@gmail.com>
2025-01-13 21:33:52 +0100

    modified:   dashboard/kubernetes.tf

--- dashboard/kubernetes.tf
+++ dashboard/kubernetes.tf
@@ -29,5 +29,5 @@ data "kubernetes_secret" "oauth2_proxy_token" {
 
 
 locals {
-  bearer_token = base64decode(data.kubernetes_secret.oauth2_proxy_token.data["token"])
+  bearer_token = base64decode(tostring(data.kubernetes_secret.oauth2_proxy_token.data["token"]))
 }
