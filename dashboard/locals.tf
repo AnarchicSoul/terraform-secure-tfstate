a93949936c7727be1a2b25a6d9ff3d947ebb5d70
AnarchicSoul <duval.johan.91@gmail.com>
2025-01-13 22:07:03 +0100

    modified:   dashboard/locals.tf

--- dashboard/locals.tf
+++ dashboard/locals.tf
@@ -218,7 +218,9 @@ ingress:
   annotations:
   #   kubernetes.io/ingress.class: nginx
   #   custom.bearer.token: "${local.bearer_token}"
+    nginx.ingress.kubernetes.io/auth-response-headers: authorization
     nginx.ingress.kubernetes.io/configuration-snippet: |
+      auth_request_set ${local.bearer_token} http://dashboard-kong-proxy.${var.namespace}.svc.cluster.local
       add_header Authorization ${local.bearer_token};
   tls: 
     - secretName: wildcard-cert
