661bc87bc77a784b0cde6696b8edfe2feaf21d1c
AnarchicSoul <duval.johan.91@gmail.com>
2025-01-13 21:31:19 +0100

    modified:   dashboard/kubernetes.tf

--- dashboard/kubernetes.tf
+++ dashboard/kubernetes.tf
@@ -3,19 +3,31 @@ resource "kubernetes_service_account" "oauth2_proxy" {
     name      = "oauth2-proxy"
     namespace = var.namespace
   }
+  automount_service_account_token = true
 }
 
+resource "kubernetes_secret" "oauth2_proxy_token" {
+  metadata {
+    name      = "oauth2-proxy-token"
+    namespace = var.namespace
+    annotations = {
+      "kubernetes.io/service-account.name" = kubernetes_service_account.oauth2_proxy.metadata[0].name
+    }
+  }
 
-data "kubernetes_secret" "oauth2_proxy" {
-  depends_on = [kubernetes_service_account.oauth2_proxy]
+  type = "kubernetes.io/service-account-token"
+}
+
+data "kubernetes_secret" "oauth2_proxy_token" {
+  depends_on = [kubernetes_secret.oauth2_proxy_token]
 
   metadata {
+    name      = kubernetes_secret.oauth2_proxy_token.metadata[0].name
     namespace = var.namespace
-    name      = kubernetes_service_account.oauth2_proxy.secrets[0].name
   }
 }
 
 
 locals {
-  bearer_token = base64decode(data.kubernetes_secret.oauth2_proxy.data["token"])
+  bearer_token = base64decode(data.kubernetes_secret.oauth2_proxy_token.data["token"])
 }
